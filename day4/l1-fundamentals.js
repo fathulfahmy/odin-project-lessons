53cbcb15139b95af966a603549fc12a3ac5f5376
fathulfahmy <fathulfmy@gmail.com>
2023-11-02 20:33:18 +0800

    day4: fundamental js 1

---
+++ day4/l1-fundamentals.js
@@ -0,0 +1,20 @@
+// VARIABLES
+var x;
+let y;
+const z = 5;
+
+// OPERATIONS
+x = 1 + 2 - 3;
+y = ((1 * 2) / 3) % 4;
+x ** 2;
+x++;
+x--;
+
+// ASSIGNMENT
+x += 2;
+x -= 2;
+x *= 2;
+x /= 2;
+
+let num = "32";
+num = Number(num) + 3;
