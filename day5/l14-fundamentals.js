0d6da47b84fafcd814a54ca4ebcf64517cbd1fa1
fathulfahmy <fathulfmy@gmail.com>
2023-11-05 12:14:01 +0800

    day7: add lesson 14 fundamentals 5 objects

---
+++ day5/l14-fundamentals.js
@@ -0,0 +1,30 @@
+// declaring obejct with {} literal
+let hobby = prompt("What is your hobby?");
+let user = {
+	name: "John",
+	age: 18,
+	["hobby"]: true,
+};
+
+// declaring object with constructor
+let player = new Object();
+player.name = "Jack";
+player.age = 20;
+
+function createRobot(name, age) {
+	return {
+		name,
+		age,
+	};
+}
+let robot = createRobot("Anthony", "2");
+
+// accessing object property
+let key = prompt("What do you want to know about the user?");
+console.log(user.key);
+console.log(user[key]);
+
+for (let key in user) {
+	console.log(key);
+	console.log(user[key]);
+}
