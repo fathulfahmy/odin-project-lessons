de63a7332d6d7a0a25cb0705cb04025cdca0b8a1
fathulfahmy <fathulfmy@gmail.com>
2023-11-09 14:34:39 +0800

    day11: add lesson 6 esmodule

---
+++ day11-js/l6-import.js
@@ -0,0 +1,7 @@
+import { functionOne, functionTwo } from "l6-module.js";
+import myDefault from "l6-module.js";
+// since default is not named, it can be named to anything
+
+functionOne();
+functionTwo();
+myDefault();
---
+++ day11-js/l6-module.js
@@ -0,0 +1,12 @@
+function functionOne() {
+	console.log("Hello");
+}
+function functionTwo() {
+	console.log("World");
+}
+function functionThree() {
+	console.log("!!!");
+}
+
+export { functionOne, functionTwo };
+export default functionThree;
