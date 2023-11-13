dec0f2f5d94b7e250b99ebab5f087968ce4eec83
fathulfahmy <fathulfmy@gmail.com>
2023-11-13 22:36:56 +0800

    day15: change gitbasics.md to .sh

--- day15-git/l2-basics.sh
+++ day15-git/l2-basics.sh
@@ -1,21 +1,21 @@
 git clone git@github.com:fathulfahmy/odin-project-lessons
-<!-- duplicate repo to local -->
+# duplicate repo to local
 
 git remote -v
-<!-- display repo location -->
+# display repo location
 
 git add .
-<!-- add file to staging area -->
+# add file to staging area
 
 git commit -m 'commit message goes here'
-<!-- add staged file to commit stage with a descriptive message -->
+# add staged file to commit stage with a descriptive message
 
 git push
 git push origin main
-<!-- add committed file to repo -->
+# add committed file to repo
 
 git status
-<!-- display status of current staging area -->
+# display status of current staging area
 
 git log
-<!-- display past commits -->
+# display past commits
