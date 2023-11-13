115ccf431e5f5ddf2dbbaf35bf94df16745cad81
fathulfahmy <fathulfmy@gmail.com>
2023-11-13 23:46:37 +0800

    day15: update git remote

--- day15-git/l4-remotes.sh
+++ day15-git/l4-remotes.sh
@@ -7,3 +7,10 @@ git push --force-with-lease
 git revert HEAD
 git push origin main
 # revert changes to HEAD and push new commit
+
+git amend
+git reset
+# only use for staged commit and not pushed commit
+
+git rebase
+# only use for branch with no other users
