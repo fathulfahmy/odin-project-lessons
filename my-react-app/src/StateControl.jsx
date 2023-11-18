6c666efe81a7fd1dd000540fff3f0e74524597e2
fathulfahmy <fathulfmy@gmail.com>
2023-11-18 23:40:26 +0800

    day19: add react more state lesson

---
+++ my-react-app/src/StateControl.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+function StateControl() {
+  const [person, setPerson] = useState({ name: "John", age: 100 });
+
+  const handleIncreaseAge = () => {
+    // copy the existing person object into a new object while updating the age property
+    const newPerson = { ...person, age: person.age + 1 };
+
+    setPerson(newPerson);
+
+    // newPerson is not latest state but initial state
+    setPerson(newPerson);
+  };
+
+  const handleIncreaseAgeTwice = () => {
+    // passing callback function to pass the latest state
+    setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1 }));
+    setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1 }));
+  };
+
+  return (
+    <>
+      <h1>{person.name}</h1>
+      <h2>{person.age}</h2>
+      <button onClick={handleIncreaseAge}>Increase age</button>
+      <button onClick={handleIncreaseAgeTwice}>Increase age twice</button>
+
+      {/* control components by updating native HTML internal state*/}
+      <input type="text" value={person.age} onChange={(event)=>{event.target.value}} />
+    </>
+  );
+}
+
+export default StateControl;
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+// sorted by date learned
 import Greeting from "./Greeting.jsx";
 import ConvertToJsx from "./ConvertToJsx.jsx";
 import Animals from "./Animals.jsx";
 import PassingData from "./PassingData.jsx";
 import IntroToState from "./IntroToState.jsx";
+import StateControl from "./StateControl.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -14,5 +16,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Animals />
     <PassingData text="Click me!" fontSize={12} />
     <IntroToState />
+    <StateControl />
   </React.StrictMode>,
 );
